0056d928c7f7bad4abcad2550f2cd356e1e6d917
Minju Son <75062110+mango606@users.noreply.github.com>
2023-12-05 20:23:01 +0900

    [level 2] Title: 타겟 넘버, Time: 5.35 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,9 +1,6 @@
 def dfs(numbers, target, depth, sum):
     if depth == len(numbers):
-        if sum == target:
-            return 1
-        else:
-            return 0
+        return sum == target
         
     return dfs(numbers, target, depth+1, sum+numbers[depth]) + dfs(numbers, target, depth+1, sum-numbers[depth])
 
