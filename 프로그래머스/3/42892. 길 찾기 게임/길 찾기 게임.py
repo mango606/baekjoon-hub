823e1060a3ec397ed23dac2eb5ebec3307fbabec
Minju Son <75062110+mango606@users.noreply.github.com>
2024-02-29 11:50:35 +0900

    [level 3] Title: 길 찾기 게임, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,26 +1,45 @@
-from sys import setrecursionlimit
-setrecursionlimit(10000)
+import sys
+sys.setrecursionlimit(10**6)  # 파이썬 기본 재귀 한도를 늘림
+
+class Node:
+    def __init__(self, data, x, y):
+        self.data = data
+        self.x = x
+        self.y = y
+        self.left = None
+        self.right = None
+
+def build_tree(nodes):
+    if not nodes:
+        return None
+    root = nodes.pop(0)
+    root_node = Node(root[0], root[1], root[2])
+    left_nodes = [node for node in nodes if node[1] < root_node.x]
+    right_nodes = [node for node in nodes if node[1] > root_node.x]
+    root_node.left = build_tree(left_nodes)
+    root_node.right = build_tree(right_nodes)
+    return root_node
+
+def preorder_traversal(node, result):
+    if node:
+        result.append(node.data)
+        preorder_traversal(node.left, result)
+        preorder_traversal(node.right, result)
+
+def postorder_traversal(node, result):
+    if node:
+        postorder_traversal(node.left, result)
+        postorder_traversal(node.right, result)
+        result.append(node.data)
 
 def solution(nodeinfo):
-    nodeinfo = sorted([(x, y, i+1) for i, (x, y) in enumerate(nodeinfo)], key=lambda x: x[0])
-    result = [[], []]
-
-    def REC(nodeinfo):
-        if nodeinfo:
-            
-            highest = (0, -1, 0)
-
-            for idx, (x, y, n) in enumerate(nodeinfo):
-                if y > highest[1]:
-                    highest = (idx, y, n)
-
-            result[0].append(highest[-1])
-
-            left, right = nodeinfo[:highest[0]], nodeinfo[highest[0]+1:]
-
-            REC(left), REC(right)
-
-            result[1].append(highest[-1])
-
-    REC(nodeinfo)
-    return result
+    nodes_with_info = [(i+1, x, y) for i, (x, y) in enumerate(nodeinfo)]
+    sorted_nodes = sorted(nodes_with_info, key=lambda x: (-x[2], x[1]))
+    
+    root = build_tree(sorted_nodes)
+    
+    preorder_result, postorder_result = [], []
+    preorder_traversal(root, preorder_result)
+    postorder_traversal(root, postorder_result)
+    
+    return [preorder_result, postorder_result]
