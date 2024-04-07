050f01ccffc4bcbb6174961fe745100ae9948809
Minju Son <75062110+mango606@users.noreply.github.com>
2024-04-07 21:29:43 +0900

    [Gold IV] Title: 비밀번호 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/17218. 비밀번호 만들기/비밀번호 만들기.py
@@ -0,0 +1,16 @@
+def solution(s1, s2):
+    dp = [['' for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
+    
+    for i in range(1, len(s1)+1):
+        for j in range(1, len(s2)+1):
+            if s1[i-1] == s2[j-1]:
+                dp[i][j] = dp[i-1][j-1] + s1[i-1]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1], key=len)
+                
+    return dp[-1][-1]
+
+s1 = input()
+s2 = input()
+
+print(solution(s1, s2))
