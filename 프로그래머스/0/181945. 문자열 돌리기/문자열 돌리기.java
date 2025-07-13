8a3e0911affdde941d6abfa28ea97f699a29d670
Minju Son <yuj08184@kangwon.ac.kr>
2025-07-13 18:45:49 +0900

    [level 0] Title: 문자열 돌리기, Time: 193.82 ms, Memory: 59.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        
+        for (char c : s.toCharArray()) {
+            System.out.println(c);
+        }
+    }
+}
