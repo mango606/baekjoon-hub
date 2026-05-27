197b96752cda693f23b9d5d7d87b6c27485834bb
MINJU SON <yuj08184@kangwon.ac.kr>
2026-05-27 18:03:34 +0900

    [Passed] 수 합치기 (373ms, 20MB)

---
+++ 코드트리/trail5/수 합치기/merge-numbers.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine()); 
+        
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            pq.add(Long.parseLong(st.nextToken()));
+        }
+        
+        long totalCost = 0;
+        
+        while (pq.size() > 1) {
+            long first = pq.poll();
+            long second = pq.poll();
+            
+            long sum = first + second;
+            
+            totalCost += sum;
+            pq.add(sum);
+        }
+        
+        System.out.println(totalCost);
+    }
+}
